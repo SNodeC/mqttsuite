286c16665517274336572b4b2969c69d47847a16
Volker Christian <me@vchrist.at>
2023-12-08 08:23:03 +0100

    Use the template function startServer

--- mqttbroker/mqttbroker.cpp
+++ mqttbroker/mqttbroker.cpp
@@ -20,19 +20,26 @@
 #include "lib/Mqtt.h"
 #include "lib/MqttModel.h"
 
-#include <core/SNodeC.h>
-#include <express/legacy/in/WebApp.h>
-#include <express/tls/in/WebApp.h>
-#include <net/in/stream/legacy/SocketServer.h> // IWYU pragma: keep
-#include <net/in/stream/tls/SocketServer.h>    // IWYU pragma: keep
-#include <net/un/stream/legacy/SocketServer.h> // IWYU pragma: keep
-#include <utils/Config.h>
+namespace core::socket::stream {
+    class SocketContextFactory;
+}
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 
+#include <core/SNodeC.h>
 #include <cstdlib>
+#include <express/legacy/in/WebApp.h>
+#include <express/tls/in/WebApp.h>
 #include <log/Logger.h>
+#include <net/in/stream/legacy/SocketServer.h> // IWYU pragma: keep
+#include <net/in/stream/tls/SocketServer.h>    // IWYU pragma: keep
+#include <net/un/stream/legacy/SocketServer.h> // IWYU pragma: keep
 #include <type_traits>
+#include <utility>
+#include <utils/Config.h>
+#include <variant>
+
+// IWYU pragma: no_include <bits/utility.h>
 
 #endif
 
@@ -103,6 +110,42 @@ void reportState(const std::string& instanceName, const SocketAddress& socketAdd
     }
 }
 
+template <template <typename, typename...> typename SocketServer,
+          typename SocketContextFactory,
+          typename... SocketContextFactoryArgs,
+          typename = std::enable_if_t<std::is_base_of_v<core::socket::stream::SocketContextFactory, SocketContextFactory>>>
+void startServer(const std::string& instanceName,
+                 const std::function<void(typename SocketServer<SocketContextFactory>::Config&)>& configurator,
+                 SocketContextFactoryArgs&&... socketContextFactoryArgs) {
+    using Server = SocketServer<SocketContextFactory, SocketContextFactoryArgs&&...>;
+    using SocketAddress = typename Server::SocketAddress;
+
+    const Server server(instanceName, std::forward<SocketContextFactoryArgs>(socketContextFactoryArgs)...);
+
+    configurator(server.getConfig());
+
+    server.listen([instanceName](const SocketAddress& socketAddress, const core::socket::State& state) -> void {
+        reportState(instanceName, socketAddress, state);
+    });
+}
+
+template <template <typename, typename...> typename SocketServer,
+          typename SocketContextFactory,
+          typename... SocketContextFactoryArgs,
+          typename = std::enable_if_t<std::is_base_of_v<core::socket::stream::SocketContextFactory, SocketContextFactory>>,
+          typename = std::enable_if_t<not std::is_invocable_v<std::tuple_element_t<0, std::tuple<SocketContextFactoryArgs...>>,
+                                                              typename SocketServer<SocketContextFactory>::Config&>>>
+void startServer(const std::string& instanceName, SocketContextFactoryArgs&&... socketContextFactoryArgs) {
+    using Server = SocketServer<SocketContextFactory, SocketContextFactoryArgs&&...>;
+    using SocketAddress = typename Server::SocketAddress;
+
+    Server server(instanceName, std::forward<SocketContextFactoryArgs>(socketContextFactoryArgs)...);
+
+    server.instance([instanceName](const SocketAddress& socketAddress, const core::socket::State& state) -> void {
+        reportState(instanceName, socketAddress, state);
+    });
+}
+
 int main(int argc, char* argv[]) {
     utils::Config::add_string_option("--mqtt-mapping-file", "MQTT mapping file (json format) for integration", "[path]", "");
     utils::Config::add_string_option("--mqtt-session-store", "Path to file for the persistent session store", "[path]", "");
@@ -110,39 +153,21 @@ int main(int argc, char* argv[]) {
     core::SNodeC::init(argc, argv);
 
     setenv("MQTT_SESSION_STORE", utils::Config::get_string_option_value("--mqtt-session-store").data(), 0);
-    {
-        using MqttBroker = net::in::stream::legacy::SocketServer<mqtt::mqttbroker::SharedSocketContextFactory>;
-        using SocketAddress = MqttBroker::SocketAddress;
 
-        const MqttBroker mqttBroker("in-mqtt");
-        mqttBroker.getConfig().setRetry();
-        mqttBroker.listen(1883, [](const SocketAddress& socketAddress, const core::socket::State& state) -> void {
-            reportState("in-mqtt", socketAddress, state);
-        });
-    }
+    startServer<net::in::stream::legacy::SocketServer, mqtt::mqttbroker::SharedSocketContextFactory>("in-mqtt", [](auto& config) -> void {
+        config.setPort(1883);
+        config.setRetry();
+    });
 
-    {
-        using MqttBroker = net::in::stream::tls::SocketServer<mqtt::mqttbroker::SharedSocketContextFactory>;
-        using SocketAddress = MqttBroker::SocketAddress;
+    startServer<net::in::stream::tls::SocketServer, mqtt::mqttbroker::SharedSocketContextFactory>("in-mqtts", [](auto& config) -> void {
+        config.setPort(8883);
+        config.setRetry();
+    });
 
-        const MqttBroker mqttBroker("in-mqtts");
-        mqttBroker.getConfig().setRetry();
-        mqttBroker.listen(8883, [](const SocketAddress& socketAddress, const core::socket::State& state) -> void {
-            reportState("in-mqtts", socketAddress, state);
-        });
-    }
-
-    {
-        using MqttBroker = net::un::stream::legacy::SocketServer<mqtt::mqttbroker::SharedSocketContextFactory>;
-        using SocketAddress = MqttBroker::SocketAddress;
-
-        const MqttBroker mqttBroker("un-mqtt");
-        mqttBroker.getConfig().setRetry();
-        mqttBroker.listen("/tmp/" + utils::Config::getApplicationName(),
-                          [](const SocketAddress& socketAddress, const core::socket::State& state) -> void {
-                              reportState("un-mqtt", socketAddress, state);
-                          });
-    }
+    startServer<net::un::stream::legacy::SocketServer, mqtt::mqttbroker::SharedSocketContextFactory>("un-mqtt", [](auto& config) -> void {
+        config.setSunPath("/tmp/" + utils::Config::getApplicationName());
+        config.setRetry();
+    });
 
     {
         using MqttBroker = express::legacy::in::WebApp;
