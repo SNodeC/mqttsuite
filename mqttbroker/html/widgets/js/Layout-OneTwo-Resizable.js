2f1ebebcab9f9b6646f6473cd2cc7c746f39cf86
Volker Christian <me@vchrist.at>
2025-04-18 17:10:19 +0200

    Polish

--- mqttbroker/html/widgets/js/Layout-OneTwo-Resizable.js
+++ mqttbroker/html/widgets/js/Layout-OneTwo-Resizable.js
@@ -1,20 +1,21 @@
-const splitter = document.getElementById('splitter');
-const topDiv = document.getElementById('drag');
-const container = document.getElementById('container');
-let isDragging = false;
-splitter.addEventListener('mousedown', function(e) {
-    e.preventDefault();
-    isDragging = true;
-    document.body.style.cursor = 'row-resize';
-});
-document.addEventListener('mousemove', function(e) {
-    e.preventDefault();
-    if (!isDragging) return;
-    const containerRect = container.getBoundingClientRect();
-    const newHeight = 100 * (e.clientY - containerRect.top - 22) / containerRect.height;
-    topDiv.style.height = `${newHeight}%`;
-});
-document.addEventListener('mouseup', function() {
-    isDragging = false;
-    document.body.style.cursor = 'default';
-});
+const splitter = document.getElementById('splitter')
+const topDiv = document.getElementById('drag')
+const container = document.getElementById('container')
+let isDragging = false
+splitter.addEventListener('mousedown', function (e) {
+    e.preventDefault()
+    isDragging = true
+    document.body.style.cursor = 'row-resize'
+})
+document.addEventListener('mousemove', function (e) {
+    e.preventDefault()
+    if (isDragging) {
+        const containerRect = container.getBoundingClientRect()
+        const newHeight = 100 * (e.clientY - containerRect.top - 22) / containerRect.height
+        topDiv.style.height = `${newHeight}%`
+    }
+})
+document.addEventListener('mouseup', function () {
+    isDragging = false
+    document.body.style.cursor = 'default'
+})
