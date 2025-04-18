44d7a168594db223f04b78cb7526a1c1f93465c8
Volker Christian <me@vchrist.at>
2025-04-18 16:15:54 +0200

    Polish

--- mqttbroker/html/widgets/js/FoldableTable.js
+++ mqttbroker/html/widgets/js/FoldableTable.js
@@ -1,11 +1,11 @@
 // Expand/collapse logic
-document.querySelectorAll('.key-row').forEach(row => {
+document.currentScript.parentElement.querySelectorAll('.key-row').forEach(row => {
     row.addEventListener('click', () => {
-        row.classList.toggle('open');
-        let next = row.nextElementSibling;
+        row.classList.toggle('open')
+        let next = row.nextElementSibling
         while (next && next.classList.contains('value-row')) {
-            next.style.display = row.classList.contains('open') ? 'table-row' : 'none';
-            next = next.nextElementSibling;
+            next.style.display = row.classList.contains('open') ? 'table-row' : 'none'
+            next = next.nextElementSibling
         }
-    });
-});
+    })
+})
