88133c6cfbab0ea7890f464b6c238069cb2e7acc
Volker Christian <me@vchrist.at>
2026-02-11 14:10:26 +0100

    Polish

--- cmake/doxygen.cmake
+++ cmake/doxygen.cmake
@@ -73,7 +73,6 @@ if(DOXYGEN_FOUND)
         add_custom_target(
             doc-fast
             COMMAND rm -f ${SNODEC_DOC_ROOTDIR}/inline_umlgraph_cache_all.pu
-            COMMAND mkdir -p ${SNODEC_DOC_ROOTDIR}/html/docs/
             COMMAND mkdir -p ${SNODEC_DOC_ROOTDIR}/html/docs/assets/README
             COMMAND cp -a ${CMAKE_SOURCE_DIR}/docs/assets/README
                     ${SNODEC_DOC_ROOTDIR}/html/docs/assets
