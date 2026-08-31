e2288ca8055d2e9b344a9c4d4520f8781634d002
Volker Christian <me@vchrist.at>
2026-08-31 16:37:28 +0200

    Remove default Integrator administration credentials

--- lib/MappingAdminRouter.h
+++ lib/MappingAdminRouter.h
@@ -3,40 +3,10 @@
  * Copyright (C) Tobias Pfeil
  *               2025, 2026
  *
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <https://www.gnu.org/licenses/>.
- */
-
-/*
- * MIT License
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  */
 
 #ifndef MQTTBROKER_LIB_MAPPINGADMINROUTER_H
@@ -59,8 +29,8 @@ namespace mqtt::lib {
 namespace mqtt::lib::admin {
 
     struct AdminOptions {
-        std::string user{"admin"};
-        std::string pass{"admin"};
+        std::string user;
+        std::string pass;
         std::string realm{"mqttsuite-admin"};
     };
 
@@ -71,10 +41,8 @@ namespace mqtt::lib::admin {
         std::size_t unsubscribed{0};
     };
 
-    // Callback to trigger reload in the main application
     using ReloadCallback = std::function<ReloadResult(bool)>;
 
-    // Creates and returns a Router that handles /config/* endpoints.
     express::Router makeMappingAdminRouter(ConfigApplication* configApplication, const AdminOptions& opt, ReloadCallback onDeploy = {});
 
 } // namespace mqtt::lib::admin
