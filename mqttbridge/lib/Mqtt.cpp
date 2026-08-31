10fa91a3f1d7d95969bab416f240e63fe2e1f884
Volker Christian <me@vchrist.at>
2026-08-31 16:33:04 +0200

    Stop logging MQTT bridge passwords

--- mqttbridge/lib/Mqtt.cpp
+++ mqttbridge/lib/Mqtt.cpp
@@ -7,36 +7,6 @@
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option)
  * any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <https://www.gnu.org/licenses/>.
- */
-
-/*
- * MIT License
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
  */
 
 #include "Mqtt.h"
@@ -58,7 +28,7 @@
 namespace mqtt::bridge::lib {
 
     Mqtt::Mqtt(const std::string& connectionName, Broker& broker)
-        : iot::mqtt::client::Mqtt(connectionName, //
+        : iot::mqtt::client::Mqtt(connectionName,
                                   broker.getClientId(),
                                   broker.getKeepAlive(),
                                   broker.getSessionStoreFileName())
@@ -72,7 +42,7 @@ namespace mqtt::bridge::lib {
         mqttsuite::semantic::bridgeLog().debug() << "  Will QoS: " << static_cast<uint16_t>(broker.getWillQoS());
         mqttsuite::semantic::bridgeLog().debug() << "  Will Retain " << broker.getWillRetain();
         mqttsuite::semantic::bridgeLog().debug() << "  Username: " << broker.getUsername();
-        mqttsuite::semantic::bridgeLog().debug() << "  Password: " << broker.getPassword();
+        mqttsuite::semantic::bridgeLog().debug() << "  Password configured: " << !broker.getPassword().empty();
         mqttsuite::semantic::bridgeLog().debug() << "  Loop Prevention: " << broker.getLoopPrevention();
     }
 
@@ -95,7 +65,6 @@ namespace mqtt::bridge::lib {
 
     void Mqtt::onDisconnected() {
         mqtt::bridge::lib::BridgeStore::instance().mqttDisconnected(broker, this);
-
         mqttsuite::semantic::bridgeLog().debug() << "MQTT: Disconnected";
     }
 
@@ -104,14 +73,12 @@ namespace mqtt::bridge::lib {
             << "MQTT: On Exit due to '" << strsignal(signum) << "' (SIG" << utils::system::sigabbrev_np(signum) << " = " << signum << ")";
 
         sendDisconnect();
-
         return Super::onSignal(signum);
     }
 
     void Mqtt::onConnack(const iot::mqtt::packets::Connack& connack) {
         if (connack.getReturnCode() == 0) {
             mqtt::bridge::lib::BridgeStore::instance().mqttConnected(broker, this);
-
             sendSubscribe(broker.getTopics());
         }
     }
