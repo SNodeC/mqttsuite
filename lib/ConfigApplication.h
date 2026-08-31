47eace42ea85ef59d7633b8bc2f065b1fea1ad5d
Volker Christian <me@vchrist.at>
2026-08-31 16:36:21 +0200

    Add non-echoing administration authentication configuration

--- lib/ConfigApplication.h
+++ lib/ConfigApplication.h
@@ -3,40 +3,10 @@
  * Copyright (C) Volker Christian <me@vchrist.at>
  *               2022, 2023, 2024, 2025, 2026
  *
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <https://www.gnu.org/licenses/>.
- */
-
-/*
- * MIT License
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  */
 
 #ifndef APPS_MQTTBROKER_MQTTBRIDGE_CONFIGBRIDGE_H
@@ -75,14 +45,23 @@ namespace mqtt::lib {
 
         bool persistMapping() const;
 
+        [[nodiscard]] std::string getAdminUser() const;
+        [[nodiscard]] std::string getAdminPassword() const;
+        [[nodiscard]] std::string getAdminRealm() const;
+        [[nodiscard]] bool hasAdminAuthentication() const;
+
     private:
         bool loadMapping(const std::string mapFilename); // can throw
+        [[nodiscard]] std::string readAdminPassword() const;
 
     protected:
         std::shared_ptr<MqttMapper> mqttMapper;
 
         CLI::Option* mappingFileOpt;
         CLI::Option* sessionStoreOpt;
+        CLI::Option* adminUserOpt;
+        CLI::Option* adminPasswordFileOpt;
+        CLI::Option* adminRealmOpt;
 
     private:
         std::string mapFilename;
